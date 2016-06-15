1a16e7b2a9ed07c41674307c3d6b8a01f15cab9f
Tom Bijesse <bijesse.thomas@gmail.com>
2016-06-15 20:22:05 +0000

    customized to ScriptEd

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //Write all your jQuery code here
+        $("button").click(function() {
+      $("#like").html("You like this project!");
+    });
+    
+    
+    
+    
+});
